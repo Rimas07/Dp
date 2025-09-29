cab7439100c541cd7f41392d72e7973bf351fd3f
Rimas07 <53482924+Rimas07@users.noreply.github.com>
2025-09-29 19:23:20 +0200

    Initial commit: HIS system with Docker support

--- His/src/config/config.ts
+++ His/src/config/config.ts
@@ -6,7 +6,7 @@ export default () => ({
         
     },
     database: {
-        connectionString: process.env.DB_CONNECTION_STRING,
+        connectionString: process.env.DB_CONNECTION_STRING || 'mongodb://host.docker.internal:27017/master',
     },
     security: {
         encryptionSecretKey: process.env.ENCRYPTION_KEY,
